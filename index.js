8e14ee2928ffa4d3fec94146491c182c7bffb1a8
wafaboussada <wafaboussada@gmail.com>
2022-05-22 23:48:00 +0100

    firstcommit

---
+++ index.js
@@ -0,0 +1,13 @@
+const btMoins=document.getElementById('btMoins');
+const btPlus=document.getElementById('btPlus');
+const nb=document.getElementById('nb');
+btMoins.addEventListener('click', function(){
+    if(parseInt(nb.innerHTML) !== 0) {
+        console.log('hello', nb.innerHTML, nb.innerHTML !== 0,
+        typeof(nb.innerHTML))
+        nb.innerHTML--;
+    }
+});
+btPlus.addEventListener('click', function(){
+    nb.innerHTML++;
+})
