8c8ecd552dc76cbe230355bed34d355f3386c84b
wafaboussada <wafaboussada@gmail.com>
2022-05-23 00:06:16 +0100

    updated

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const btMoins=document.getElementById('btMoins');
-const btPlus=document.getElementById('btPlus');
-const nb=document.getElementById('nb');
-btMoins.addEventListener('click', function(){
-    if(parseInt(nb.innerHTML) !== 0) {
+const btMoins = document.getElementById('btMoins');
+const btPlus = document.getElementById('btPlus');
+const nb = document.getElementById('nb');
+btMoins.addEventListener('click', function() {
+    if (parseInt(nb.innerHTML) !== 0) {
         console.log('hello', nb.innerHTML, nb.innerHTML !== 0,
-        typeof(nb.innerHTML))
+            typeof(nb.innerHTML))
         nb.innerHTML--;
+        console.log('wafa');
     }
 });
-btPlus.addEventListener('click', function(){
+btPlus.addEventListener('click', function() {
     nb.innerHTML++;
 })
