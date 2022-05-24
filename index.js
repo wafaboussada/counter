29ecfd3544a52e744972067337b609a325a4a859
wafaboussada <wafaboussada@gmail.com>
2022-05-24 23:36:53 +0100

    remove console

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const btPlus = document.getElementById('btPlus');
 const nb = document.getElementById('nb');
 btMoins.addEventListener('click', function() {
     if (parseInt(nb.innerHTML) !== 0) {
-        console.log('hello', nb.innerHTML, nb.innerHTML !== 0,
-            typeof(nb.innerHTML))
         nb.innerHTML--;
     }
 });
